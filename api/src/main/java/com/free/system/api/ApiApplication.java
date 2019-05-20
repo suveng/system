d9fb136e870a906fae6291a06b1df4d2e8487467
suveng <suveng@163.com>
2019-05-20 15:38:53 +0800

    增加docker build

---
+++ api/src/main/java/com/free/system/api/ApiApplication.java
@@ -0,0 +1,17 @@
+package com.free.system.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * description:
+ * @author suwenguang@52tt.com
+ * @date 2019/5/17
+ * @version 1.0.0
+ **/
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
---
+++ monitor/src/main/java/com/free/system/monitor/MonitorApplication.java
@@ -0,0 +1,17 @@
+package com.free.system.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * description:
+ * @author suwenguang@52tt.com
+ * @date 2019/5/17
+ * @version 1.0.0
+ **/
+@SpringBootApplication
+public class MonitorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorApplication.class, args);
+    }
+}
