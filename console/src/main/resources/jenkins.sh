dc5a67ec51b6a8eed6d6769ffadb7ab8f5a6a902
suveng <suveng@163.com>
2019-05-22 20:00:06 +0800

    更新脚本

--- console/src/main/resources/jenkins.sh
+++ console/src/main/resources/jenkins.sh
@@ -1,12 +1,19 @@
+echo "###########环境准备###############"
 source /etc/profile
 jps
+echo "###########停止项目###############"
 /etc/xiaobo/suveng/sh/stop-sys-console.sh
 jps
+echo "###########项目构建###############"
 cd src/system
 mvn clean
 mvn package -Dmaven.test.skip=true
 mvn install -Dmaven.test.skip=true
 cd console
 mvn package docker:build
+echo "###########项目部署###############"
+rm -rf /etc/xiaobo/suveng/app/sys-console.jar
+mv target/sys-console-1.0-SNAPSHOT.jar /etc/xiaobo/suveng/app/sys-console.jar
 BUILD_ID=dontKillMe /etc/xiaobo/suveng/sh/deploy.sh
+echo "###########结束部署###############"
 jps
